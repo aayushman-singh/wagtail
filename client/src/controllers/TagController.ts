4557199b71971963480185880b0a89bf5f06a497
Aayushman Singh <aayushman2702@gmail.com>
2024-11-05 20:11:27 +0530

    Ran prettier --write

--- client/src/controllers/TagController.ts
+++ client/src/controllers/TagController.ts
@@ -32,14 +32,17 @@ export class TagController extends Controller {
   connect() {
     const preprocessTag = this.cleanTag.bind(this);
     // Debounces by specified debounceValue to reduce unnecessary calls to database while user types, default is 300
-    const debouncedAutocomplete = debounce((request: {term : string}, response : (data : any) => void) => {
-      $.ajax({
-        url: this.urlValue,
-        dataType: 'json',
-        data: { term: request.term },
-        success: response,
-      });
-    }, this.debounceValue)
+    const debouncedAutocomplete = debounce(
+      (request: { term: string }, response: (data: any) => void) => {
+        $.ajax({
+          url: this.urlValue,
+          dataType: 'json',
+          data: { term: request.term },
+          success: response,
+        });
+      },
+      this.debounceValue,
+    );
 
     $(this.element).tagit({
       autocomplete: { source: debouncedAutocomplete },
